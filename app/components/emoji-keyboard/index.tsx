d88d220b5e1939090172746e33cf7a47f2061ccd
Aditya Srivastava <adityasrivastava@Adityas-MacBook-Pro.local>
2026-01-20 16:05:43 +0530

    :sparkle: Add Emoji Bottomsheet

---
+++ app/components/emoji-keyboard/index.tsx
@@ -0,0 +1,97 @@
+import { scale } from "@/app/utility/responsive";
+import React, { useMemo } from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import EmojiPicker, {
+  emojisByCategory,
+  EmojisByCategory,
+  EmojiType,
+} from "rn-emoji-keyboard";
+import { CategoryTypes } from "rn-emoji-keyboard/lib/typescript/types";
+import { popularEmojis } from "./popularEmojis";
+
+export type EmojiKeyboardProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  onEmojiSelected: (emoji: EmojiType) => void;
+};
+const EmojiKeyBoard = ({
+  isOpen,
+  onClose,
+  onEmojiSelected,
+}: EmojiKeyboardProps) => {
+  const { height } = Dimensions.get("window");
+  const customEmojisByCategory: EmojisByCategory[] = useMemo(() => {
+    const filteredDefault = emojisByCategory.filter(
+      (cat) => cat.title !== "recently_used",
+    );
+    return [
+      {
+        title: "recently_used" as CategoryTypes,
+        data: popularEmojis,
+      },
+      ...filteredDefault,
+    ];
+  }, []);
+
+  return (
+    <EmojiPicker
+      expandable={true}
+      emojiSize={scale.moderate(40)}
+      defaultHeight={height * 0.69}
+      open={isOpen}
+      onClose={onClose}
+      enableSearchBar={false}
+      enableRecentlyUsed={false}
+      categoryPosition="bottom"
+      //enableCategoryChangeGesture={true}
+      disabledCategories={["symbols"]}
+      emojisByCategory={customEmojisByCategory}
+      onEmojiSelected={onEmojiSelected}
+      theme={{
+        container: "red",
+        header: "#FFFFFFA3",
+        knob: "blue",
+        category: {
+          icon: "#EBEBF599",
+          iconActive: "#EBEBF599",
+          containerActive: "#7878805C",
+        },
+      }}
+      styles={{
+        header: {
+          fontFamily: "Outfit-Regular",
+          fontSize: scale.font(16),
+        },
+        knob: {
+          height: 6,
+          width: 63,
+          top: 20,
+          zIndex: 1,
+          position: "absolute",
+          backgroundColor: "#474747",
+          borderRadius: 28,
+          display: "flex",
+        },
+        container: {
+          paddingTop: scale.vertical(24),
+        },
+
+        category: {
+          container: {
+            paddingTop: scale.v(30),
+            borderTopColor: "#FFFFFF1F",
+            borderWidth: scale.m(1),
+            backgroundColor: "#232322",
+            paddingBottom: 30,
+          },
+        },
+      }}
+    />
+  );
+};
+
+export default EmojiKeyBoard;
+
+const styles = StyleSheet.create({
+  bottomsheetContainer: {},
+});
--- app/screens/consumption/index.tsx
+++ app/screens/consumption/index.tsx
@@ -1,106 +1,53 @@
-import BlurredVideo from "@/app/components/videos/BlurredVideo";
-import { useVideoPlayer, VideoView } from "expo-video";
-import React, { useEffect, useState } from "react";
-import { Dimensions, StyleSheet, TouchableOpacity, View } from "react-native";
-import { useSharedValue } from "react-native-reanimated";
-const videoSource =
-  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4";
-const ConsumptionScreen = ({}) => {
-  const videoPaused = useSharedValue(true);
+import EmojiKeyBoard from "@/app/components/emoji-keyboard";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { EmojiType } from "rn-emoji-keyboard";
 
-  const { width, height } = Dimensions.get("window");
-  const [expoReady, setExpoReady] = useState(false);
-  const [skiaReady, setSkiaReady] = useState(false);
-  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
-  const videoPlayer = useVideoPlayer(videoSource, (player) => {
-    player.loop = false;
-    player.muted = true;
-  });
-  const handleStartStop = (isVideoPlaying: boolean) => {
-    console.log("check isVideoPlaying", isVideoPlaying);
-    if (!isVideoPlaying) {
-      videoPaused.value = false;
-      videoPlayer.play();
-      setIsVideoPlaying(true);
-      console.log(" stoped");
-    } else {
-      videoPaused.value = true;
-      videoPlayer.pause();
-      setIsVideoPlaying(false);
-    }
-  };
-  useEffect(() => {
-    const subscription = videoPlayer.addListener("statusChange", (payload) => {
-      // readyToPlay means metadata is loaded and frame 0 is ready
-      if (payload.status === "readyToPlay") {
-        setExpoReady(true);
-      }
-    });
-    return () => subscription.remove();
-  }, [videoPlayer]);
+const Consumption = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedEmoji, setSelectedEmoji] = useState<EmojiType | null>(null);
 
-  useEffect(() => {
-    if (expoReady && skiaReady && !isVideoPlaying) {
-      // Play Skia
-      videoPaused.value = false;
+  console.log(selectedEmoji, "selectedEmoji");
 
-      // Play Expo
-      videoPlayer.play();
-
-      setIsVideoPlaying(true);
-    }
-  }, [expoReady, skiaReady, videoPlayer]);
   return (
     <View style={styles.container}>
-      <BlurredVideo
-        videoPaused={videoPaused}
-        style={{
-          position: "absolute",
-          width: "100%",
-          height: height,
-        }}
-        blurRadius={20}
-        width={width}
-        height={height + 15}
-        onReady={() => setSkiaReady(true)}
-        source={videoSource}
-      />
       <TouchableOpacity
-        onPress={() => handleStartStop(isVideoPlaying)}
-        activeOpacity={0.7}
+        onPress={() => setIsOpen(true)}
         style={{
-          width: 359,
-          height: 359,
-          marginTop: 150,
-          borderWidth: 0.96,
-          borderColor: "rgba(255, 255, 255, 0.24)",
-          borderRadius: 180,
+          width: 100,
+          height: 100,
           justifyContent: "center",
           alignItems: "center",
-          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#fff",
         }}
       >
-        <VideoView
-          player={videoPlayer}
-          contentFit="cover"
-          nativeControls={false}
-          style={[StyleSheet.absoluteFill, { height: 359 }]}
+        <Text style={{ color: "white", fontSize: 20 }}>
+          {selectedEmoji ? `${selectedEmoji.emoji}` : `Press here`}
+        </Text>
+
+        <EmojiKeyBoard
+          isOpen={isOpen}
+          onClose={() => {
+            setIsOpen(false);
+          }}
+          onEmojiSelected={(emoji: EmojiType) => {
+            setSelectedEmoji(emoji);
+            setIsOpen(false);
+          }}
         />
       </TouchableOpacity>
     </View>
   );
 };
 
-export default ConsumptionScreen;
+export default Consumption;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: "center",
+    backgroundColor: "black",
+    justifyContent: "center",
     alignItems: "center",
-  },
-  videoContainer: {
-    width: 359,
-    aspectRatio: 1,
   },
 });
